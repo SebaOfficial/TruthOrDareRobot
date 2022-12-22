654eab76d3fc3a94896760a1fdc0522d730e7b1a
Sebastiano <sebastiano@racca.me>
2022-12-22 19:19:51 +0100

    Fixed repo link in credits

--- commands/credits.php
+++ commands/credits.php
@@ -5,8 +5,8 @@ Please do NOT remove this file so I can have my credits :)
 */
 
 sendMessage($chat_id, 
-    "<b>Hi there, this bot is made with this <a href='https://github.com/SebaOfficial/Obbligo_VeritaBot'>repo</a></b>\n<i>Created by @SebaOfficial</i>",
-    json_encode(['inline_keyboard' => [[['text' => '🗂 Repository', 'url' => 'https://github.com/SebaOfficial/Obbligo_VeritaBot'],['text' => '🧑‍💻 Developer', 'url' => 'https://t.me/SebaOfficial']]]])
+    "<b>Hi there, this bot is made with this <a href='https://github.com/SebaOfficial/TruthOrDareRobot/'>repo</a></b>\n<i>Created by @SebaOfficial</i>",
+    json_encode(['inline_keyboard' => [[['text' => '🗂 Repository', 'url' => 'https://github.com/SebaOfficial/TruthOrDareRobot/'],['text' => '🧑‍💻 Developer', 'url' => 'https://t.me/SebaOfficial']]]])
 );
 
 ?>
